2cbe69e4e87d71dee4227672cbdcd134de5b60be
Prakash Raj <114805760+PrakashRaj-1493@users.noreply.github.com>
2022-10-01 23:08:09 +0530

    Add files via upload

---
+++ FactorialAssignment.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class FactorialAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number=5;
+		int fact=1;
+		for (int i = 1; i <= number; i++) {
+			fact=fact*i;
+			
+		}
+		
+		System.out.println("Factorial of" + number + "is:"+fact);
+		
+	}
+
+}
